402bc07b7555c591044655d76382f473b256553f
Héctor Mendoza <ectormendoza@hotmail.com>
2017-12-22 16:18:55 -0600

    added - precio

---
+++ src/main/java/com/asetecit/controlplusmtest/core/Precio.java
@@ -0,0 +1,50 @@
+package com.asetecit.controlplusmtest.core;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Precio implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int codigo;
+	private String nombre;
+	private BigDecimal monto;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public BigDecimal getMonto() {
+		return monto;
+	}
+
+	public static class Build {
+
+		private String nombre="";
+		private BigDecimal monto=new BigDecimal(0);
+
+		public Build withNombre(String nombre) {
+			this.nombre = nombre;
+			return this;
+		}
+
+		public Build withMonto(BigDecimal monto) {
+			this.monto = monto;
+			return this;
+		}
+
+		public Precio build() {
+			return new Precio(this);
+		}
+	}
+
+	private Precio(Build b) {
+		this.nombre = b.nombre;
+		this.monto = b.monto;
+	}
+}
