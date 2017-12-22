d42a5f877762f74f1a51c18ade586331e67dc4f7
Héctor Mendoza <ectormendoza@hotmail.com>
2017-12-22 17:05:51 -0600

    changed - eliminado el builder

--- src/main/java/com/asetecit/controlplusmtest/core/Categoria.java
+++ src/main/java/com/asetecit/controlplusmtest/core/Categoria.java
@@ -2,37 +2,27 @@ package com.asetecit.controlplusmtest.core;
 
 import java.io.Serializable;
 
-public class Categoria implements Serializable{
+public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private int codigo;
 	private String nombre;
-	
+
+	public Categoria() {
+		this("General");
+	}
+
+	public Categoria(String nombre) {
+		this.nombre = nombre;
+	}
+
 	public int getCodigo() {
 		return codigo;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
-	}
-	
-	public static class Build{
-		
-		private String nombre="";
-		
-		public Build withNombre(String nombre){
-			this.nombre=nombre;
-			return this;
-		}
-		
-		public Categoria build(){
-			return new Categoria(this);
-		}	
-	}
-	
-	private Categoria(Build b) {
-		this.nombre = b.nombre;
 	}
 
 }
