4359c58438244149e93e7e1ee95e4db0a0e5efd6
Héctor Mendoza <ectormendoza@hotmail.com>
2017-12-29 15:47:57 -0600

    added - implementación repositorio categoría

---
+++ src/main/java/com/asetecit/controlplusmtest/repository/CategoriaRegistro.java
@@ -0,0 +1,44 @@
+package com.asetecit.controlplusmtest.repository;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.asetecit.controlplusmtest.core.Categoria;
+
+public class CategoriaRegistro implements CategoriaRepository {
+
+	private static EntityManager em;
+
+	public CategoriaRegistro(EntityManager em) {
+		this.em = em;
+	}
+
+	@Override
+	public Collection<Categoria> listar() {
+		@SuppressWarnings("unchecked")
+		List<Categoria> categorias = (List<Categoria>) em.createQuery("FROM Categoria").getResultList();
+		return categorias;
+	}
+
+	@Override
+	public Categoria agregar(Categoria cat) {
+		em.getTransaction().begin();
+		em.persist(cat);
+		em.getTransaction().commit();
+		return cat;
+	}
+
+	@Override
+	public Categoria actualizar(Categoria cat) {
+
+		em.find(Categoria.class, cat.getId());
+		em.getTransaction().begin();
+		cat.setNombre(cat.getNombre());
+		em.getTransaction().commit();
+
+		return cat;
+	}
+
+}
