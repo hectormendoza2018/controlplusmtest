43f40bbeb8292cf70f8c726d7ca0bd3568258602
Héctor Mendoza <ectormendoza@hotmail.com>
2018-01-10 17:32:35 -0600

    changed -- GenerationType se cambio a IDENTITY

--- src/main/java/com/asetecit/controlplusmtest/core/Existencia.java
+++ src/main/java/com/asetecit/controlplusmtest/core/Existencia.java
@@ -19,7 +19,7 @@ public class Existencia implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
 
--- src/main/java/com/asetecit/controlplusmtest/core/Producto.java
+++ src/main/java/com/asetecit/controlplusmtest/core/Producto.java
@@ -20,7 +20,7 @@ public class Producto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
 
