dbc18538dac807a78d694b567ab44a51a15f5c87
Héctor Mendoza <ectormendoza@hotmail.com>
2018-01-11 13:06:30 -0600

    added - existencia por unidades

--- src/main/java/com/asetecit/controlplusmtest/repository/ExistenciaJpaRepository.java
+++ src/main/java/com/asetecit/controlplusmtest/repository/ExistenciaJpaRepository.java
@@ -1,9 +1,13 @@
 package com.asetecit.controlplusmtest.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.asetecit.controlplusmtest.core.Existencia;
 
 public interface ExistenciaJpaRepository extends CrudRepository<Existencia, Integer> {
+
+	Collection<Existencia> findByUnidades(int existencia);
 
 }
