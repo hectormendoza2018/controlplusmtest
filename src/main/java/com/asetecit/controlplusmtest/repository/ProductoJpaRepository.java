e3e6f54c8121a5366df1f8e484597a099981b40f
Héctor Mendoza <ectormendoza@hotmail.com>
2018-01-11 15:34:53 -0600

    added - metodos para busqueda

--- src/main/java/com/asetecit/controlplusmtest/repository/ProductoJpaRepository.java
+++ src/main/java/com/asetecit/controlplusmtest/repository/ProductoJpaRepository.java
@@ -1,9 +1,20 @@
 package com.asetecit.controlplusmtest.repository;
 
+import java.math.BigDecimal;
+import java.util.Collection;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.asetecit.controlplusmtest.core.Producto;
 
 public interface ProductoJpaRepository extends PagingAndSortingRepository<Producto, Integer> {
+
+	Producto findByNombre(String nombre);
+
+	Collection<Producto> findByCategoriaId(int codigo);
+
+	Producto findByCup(String cup);
+
+	Collection<Producto> findByPrecio(BigDecimal precio);
 
 }
