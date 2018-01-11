c48abbcead814b3aa60658eca11bf73ed88e9f9d
Héctor Mendoza <ectormendoza@hotmail.com>
2018-01-11 13:11:59 -0600

    refactor - Existencia repositoy

--- src/main/java/com/asetecit/controlplusmtest/repository/ExistenciaRegistro.java
+++ src/main/java/com/asetecit/controlplusmtest/repository/ExistenciaRegistro.java
@@ -1,51 +1,46 @@
 package com.asetecit.controlplusmtest.repository;
 
 import java.util.Collection;
-import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import com.asetecit.controlplusmtest.core.BusinessException;
 import com.asetecit.controlplusmtest.core.Existencia;
+import com.google.common.collect.Lists;
 
+@Repository
 public class ExistenciaRegistro implements ExistenciaRepository {
 
-	private EntityManager em;
+	ExistenciaJpaRepository repository;
 
-	public ExistenciaRegistro(EntityManager em) {
-		this.em = em;
+	@Autowired
+	public ExistenciaRegistro(ExistenciaJpaRepository repository) {
+		this.repository = repository;
 	}
 
 	@Override
 	public Collection<Existencia> listar() {
-		@SuppressWarnings("unchecked")
-		List<Existencia> existencias = (List<Existencia>) em.createQuery("FROM Existencia").getResultList();
-		return existencias;
+		return Lists.newArrayList(repository.findAll());
 	}
 
 	@Override
 	public Existencia agregar(Existencia existencia) {
-		em.getTransaction().begin();
-		em.persist(existencia);
-		em.getTransaction().commit();
-		return existencia;
+		if (null == existencia) {
+			throw new BusinessException("La existencia no es válida.");
+		}
+		return repository.save(existencia);
 	}
 
 	@Override
 	public Existencia actualizar(Existencia existencia) {
-		em.getTransaction().begin();
-		em.merge(existencia);
-		em.getTransaction().commit();
-		return existencia;
+		return agregar(existencia);
 	}
 
 	@Override
 	public Collection<Existencia> listarEqualZero() {
-		Query query = em.createQuery("from Existencia exist where exist.unidades =:unidades");
-		query.setParameter("unidades", 0);
-		@SuppressWarnings("unchecked")
-		List<Existencia> existencias = (List<Existencia>) query.getResultList();
-		return existencias;
+
+		return repository.findByUnidades(0);
 	}
 
 }
