05b2bb20b1497000ee78837a8db6fa57b720d594
Alvaro De León <orochies@gmail.com>
2018-01-23 18:24:12 -0600

    added - search product by category, cup and price

--- src/main/java/com/asetecit/controlplusmtest/controller/ProductoController.java
+++ src/main/java/com/asetecit/controlplusmtest/controller/ProductoController.java
@@ -1,5 +1,9 @@
 package com.asetecit.controlplusmtest.controller;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,10 +43,36 @@ public class ProductoController {
 	}
 
 	@GetMapping("/search")
-	public Producto search(@RequestParam(defaultValue = "") String query) {
+	public Collection<Producto> search(@RequestParam(defaultValue = "") String query,
+			@RequestParam(required = false, defaultValue = "") String type) {
 
-		log.debug("Finding producto with nombre {}", query);
-		return repository.buscar(query);
+		log.debug("Finding producto by {} with query [{}]", type, query);
+		Collection<Producto> products = new ArrayList<>();
+
+		switch (type) {
+		case "category":
+			try {
+				products.addAll(repository.buscarPorCategoria(Integer.parseInt(query)));
+			} catch (NumberFormatException e) {
+
+			}
+			break;
+		case "cup":
+			products.add(repository.buscarPorCup(query));
+			break;
+		case "price":
+			try {
+				products.addAll(repository.buscarPorPrecio(new BigDecimal(query)));
+			} catch (NumberFormatException e) {
+
+			}
+			break;
+		default:// by nombre
+			products.add(repository.buscar(query));
+			break;
+		}
+
+		return products;
 	}
 
 }
